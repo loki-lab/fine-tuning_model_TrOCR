971e32a6b6c5d947c5cf63d6e16cf254f22247bf
Loke <nguyenvanminhcuong2002@gmail.com>
2024-02-13 00:30:21 +0700

    commit

--- model_kit/config.py
+++ model_kit/config.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class TrainingConfig:
-    batch_size: int = 16
+    batch_size: int = 32
     epochs: int = 35
     learning_rate: float = 0.00005
 
